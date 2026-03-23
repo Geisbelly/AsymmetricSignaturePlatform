08d57c68b042cc239b96b4e288210cbfe7e75a04
Geisbelly Victória <geisbelly19@gmail.com>
2026-03-23 11:45:44 -0300

    porta de acesso definit

--- app.py
+++ app.py
@@ -525,5 +525,7 @@ def index():
 
 
 if __name__ == "__main__":
+    import os
     init_db(app)
-    app.run(debug=True)
+    porta = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=porta, debug=True)
